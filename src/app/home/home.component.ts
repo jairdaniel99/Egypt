20dfb59d74cb6e29ee739000c9912dbaecb99bc7
jairdaniel99 <160685574+jairdaniel99@users.noreply.github.com>
2024-04-15 19:29:52 -0600

    create home component & style

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css',
+})
+export class HomeComponent {
+  pyramidsUrl: string =
+    'https://cdn.pixabay.com/photo/2016/02/02/18/33/sphinx-1175828__340.jpg';
+}
